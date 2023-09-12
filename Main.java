3c3e70cd766839f7d3ec47900ccd74f322d1f6ec
deva5331115 <117874077+deva5331115@users.noreply.github.com>
2023-09-12 10:58:44 +0530

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,12 @@
+import java.util.*;
+class Main{
+public static void main(String args[]){
+Scanner sc=new Scanner(System.in);
+int eng=sc.nextInt();
+int tam=sc.nextInt();
+int mat=sc.nextInt();
+int sci=sc.nextInt();
+int soc=sc.nextInt();
+System.out.println((eng+tam+mat+sci+soc)/5);
+}
+}
